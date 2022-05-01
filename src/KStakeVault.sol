0f7a6391028bba90f33a81f2ffdf4e1f90cfd8a8
Emmett Childs <emmettchilds@thegobospot.com>
2022-05-01 14:00:36 +0800

    populated IKlerosV2 interface

--- src/KStakeVault.sol
+++ src/KStakeVault.sol
@@ -2,4 +2,11 @@
 pragma solidity 0.8.10;
 
 import { VmExtended } from "../lib/vm-extended/src/VmExtended.sol";
-contract KStakeVault is VmExtended {}
+import { IKlerosV2 } from "./interfaces/IKlerosV2.sol";
+import { IERC20 } from "../lib/vm-extended/src/interfaces/IERC20.sol";
+
+contract KStakeVault is VmExtended {
+    IKlerosV2 public kleros = IKlerosV2(KLEROS_LIQ);
+    IERC20 public pnk = IERC20(KLEROS_PNK);
+    IERC20 public weth = IERC20(fetchWETH(ETHEREUM));
+}
